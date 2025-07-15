a1a83f7dc9795c84b251bb9f45d0e93fbae63a25
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-07-15 11:13:33 +0200

    [UPD] Snack 3 DONE

--- script.js
+++ script.js
@@ -5,3 +5,9 @@
 //🏆 Code Question 2
 //-1 Teoricamente, dovrebbe stampare `Salad` in console.log
 //-2 Vengono creati 3 oggetti: Oggetto `hamburger`, array `ingredients` e oggetto `secondBurger`
+
+//🏆 Code Question 3
+//-1 Teoricamente 9 in qunato structuredClone crea copie totalmente indipendenti:
+// `hamburger`: 1 oggetto + `maker` + `restaurant` = 3
+// `secondBurger`: nuova copia profonda = 3
+// `thirdBurger`: nuova copia profonda = 3
