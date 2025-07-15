2aaa790186f5e5ede67f46842fe148e84de91d30
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-07-15 11:13:19 +0200

    [UPD] Snack 2 DONE

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 //🏆 Code Question 1
 //-1 Teoricamente, dovrebbe uscire in console `Double Cheese Burger` in entrambi i console.log
 //-2 Solo 1 oggetto. `secondBurger` è solo un riferimento allo stesso oggetto `hamburger`
+
+//🏆 Code Question 2
+//-1 Teoricamente, dovrebbe stampare `Salad` in console.log
+//-2 Vengono creati 3 oggetti: Oggetto `hamburger`, array `ingredients` e oggetto `secondBurger`
