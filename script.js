8b19860852cf3e0414a4254d0becb9e5d8d968e3
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-07-15 11:05:24 +0200

    [UPD] Snack 1 DONE

---
+++ script.js
@@ -0,0 +1,3 @@
+//🏆 Code Question 1
+//-1 Teoricamente, dovrebbe uscire in console `Double Cheese Burger` in entrambi i console.log
+//-2 Solo 1 oggetto. `secondBurger` è solo un riferimento allo stesso oggetto `hamburger`
